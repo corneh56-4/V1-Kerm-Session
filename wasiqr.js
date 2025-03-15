40239273a0142c3fd827122c2ae75e433390e6b3
CORNEH TECH <cornehtech@gmail.com>
2025-03-15 09:29:36 +0300

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: 'KG_TECH_237;;;' + b64data });
+				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: 'CORNEH_TECH_254;;;' + b64data });
 	
 				   let WASI_MD_TEXT = `
 *_Session Connected By CORNEH TECH_*
